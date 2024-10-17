9316ef0a85ef3d943337dea347a9e3b594b41533
Meet Mehta <mehtameetoff2802@gmail.com>
2024-10-17 23:40:05 +0530

    ineventory status implemented

--- app/src/main/java/com/example/managerapp/ui/fragments/InventoryStatusFragment.kt
+++ app/src/main/java/com/example/managerapp/ui/fragments/InventoryStatusFragment.kt
@@ -5,14 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.managerapp.adapters.ItemAdapter
+import com.example.managerapp.adapters.OnItemInteractionListener
 import com.example.managerapp.databinding.FragmentInventoryStatusBinding
+import com.example.managerapp.models.Item
 import com.example.managerapp.ui.ManagerActivity
+import com.example.managerapp.utils.Resource
 import com.example.managerapp.viewmodel.ManagerViewModel
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.coroutines.launch
 
-class InventoryStatusFragment : Fragment() {
+class InventoryStatusFragment : Fragment(), OnItemInteractionListener {
 
     lateinit var binding: FragmentInventoryStatusBinding
     lateinit var viewModel: ManagerViewModel
+    private lateinit var rvAdapter: ItemAdapter
+    private lateinit var user: FirebaseUser
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,6 +41,82 @@ class InventoryStatusFragment : Fragment() {
 
         viewModel = (activity as ManagerActivity).viewModel
 
+        rvAdapter = ItemAdapter(emptyList(),this)
+        binding.rvInventory.layoutManager = LinearLayoutManager(context)
+        binding.rvInventory.adapter = rvAdapter
+
+        user = viewModel.getCurrentUser()!!
+
+        observeItemsResult()
+    }
+
+    override fun onUpdateStock(item: Item, newStock: Int) {
+        viewModel.updateItemStock(user.uid,item.item_id!!,newStock)
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.updateItemStockResult.collect { resource ->
+                    when (resource) {
+                        is Resource.Error -> {
+                            binding.progressBar.visibility = View.GONE
+                            println("update stock error ========= ")
+                            Toast.makeText(requireContext(), resource.message, Toast.LENGTH_LONG).show()
+                        }
+
+                        is Resource.Loading -> {
+                            binding.progressBar.visibility = View.VISIBLE
+                        }
+
+                        is Resource.StandBy -> {
+                            binding.progressBar.visibility = View.INVISIBLE
+                        }
+                        is Resource.Success -> {
+                            println("stock updated successfully ========= ")
+                            binding.progressBar.visibility = View.GONE
+//                            Toast.makeText(requireContext(),"Stock Updated", Toast.LENGTH_LONG).show()
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+
+    private fun observeItemsResult() {
+        viewModel.getAllItems(user.uid)
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.getItemResult.collect { resource ->
+                    when (resource) {
+                        is Resource.Error -> {
+                            binding.progressBar.visibility = View.GONE
+                            println("recyclerview data collection failed error ========= " + resource.data)
+                            Toast.makeText(requireContext(), resource.message, Toast.LENGTH_LONG)
+                                .show()
+                        }
+
+                        is Resource.Loading -> {
+                            binding.progressBar.visibility = View.VISIBLE
+                        }
+
+                        is Resource.StandBy -> {
+                            binding.progressBar.visibility = View.INVISIBLE
+                        }
+
+                        is Resource.Success -> {
+                            println("recyclerview data collection success ========= " + resource.data)
+                            binding.progressBar.visibility = View.GONE
+                            val filteredItems = resource.data!!.filter { item ->
+                                item.item_stock!!<item.min_quantity!!
+                            }
+                            rvAdapter.updateItems(filteredItems)
+                        }
+                    }
+
+                }
+            }
+        }
     }
 
 }

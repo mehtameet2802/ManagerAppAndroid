5177f00c8697d47e37f34bce872300785c0e004a
Meet Mehta <mehtameetoff2802@gmail.com>
2024-10-23 10:57:59 +0530

    resolving error in Transaction model

--- app/src/main/java/com/example/managerapp/models/Transaction.kt
+++ app/src/main/java/com/example/managerapp/models/Transaction.kt
@@ -1,11 +1,11 @@
 package com.example.managerapp.models
 
 data class Transaction(
-    val transaction_id:String,
-    val item_cost: Int,
-    val item_name: String,
-    val transaction_type:String,
-    val transaction_units:Int,
-    val final_stock:Int,
-    val transaction_date_time:String
+    val transaction_id:String?=null,
+    val item_cost: Int?=null,
+    val item_name: String?=null,
+    val transaction_type:String?=null,
+    val transaction_units:Int?=null,
+    val final_stock:Int?=null,
+    val transaction_date_time:String?=null
 )

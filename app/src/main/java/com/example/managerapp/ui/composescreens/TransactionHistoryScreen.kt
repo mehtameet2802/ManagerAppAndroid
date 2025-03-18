e1023fcd4e8939476087dc3055795ad03716f01d
Meet Mehta <mehtameetoff2802@gmail.com>
2025-03-19 00:00:52 +0530

    migrating TransactionHistoryFragment to compose

--- app/src/main/java/com/example/managerapp/ui/composescreens/TransactionHistoryScreen.kt
+++ app/src/main/java/com/example/managerapp/ui/composescreens/TransactionHistoryScreen.kt
@@ -1,1 +1,266 @@
 package com.example.managerapp.ui.composescreens
+
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.fragment.app.FragmentManager
+import com.example.managerapp.utils.Resource
+import com.example.managerapp.viewmodel.ManagerViewModel
+import com.google.android.material.datepicker.MaterialDatePicker
+
+// Helper function to get FragmentManager in Compose
+@Composable
+fun rememberFragmentManager(): FragmentManager {
+    val context = LocalContext.current
+    return remember {
+        (context as AppCompatActivity).supportFragmentManager
+    }
+}
+
+@Composable
+fun TransactionHistoryScreen(viewModel: ManagerViewModel) {
+
+    var startDate by rememberSaveable { mutableStateOf("") }
+    var startDateError by rememberSaveable { mutableStateOf<String?>(null) }
+    var endDate by rememberSaveable { mutableStateOf("") }
+    var endDateError by rememberSaveable { mutableStateOf<String?>(null) }
+    var isLoading by rememberSaveable { mutableStateOf(false) }
+
+    var calenderStartDate by rememberSaveable { mutableStateOf("") }
+    var calenderEndDate by rememberSaveable { mutableStateOf("") }
+
+    val user by viewModel.currentUser.collectAsState()
+
+    val context = LocalContext.current
+
+
+    fun clearFields() {
+        startDate = ""
+        endDate = ""
+    }
+
+    LaunchedEffect(viewModel.getTransactionHistoryResult) {
+        viewModel.getTransactionHistoryResult.collect { resource ->
+            when (resource) {
+                is Resource.Error -> {
+                    isLoading = false
+                    println("error getting the transactions ========= ")
+                    Toast.makeText(context, resource.message, Toast.LENGTH_LONG)
+                        .show()
+                }
+
+                is Resource.Loading -> isLoading = true
+
+                is Resource.StandBy -> isLoading = false
+
+                is Resource.Success -> {
+                    println("got transaction successfully ========= ")
+                    isLoading = false
+                    if (resource.data!!.isEmpty())
+                        Toast.makeText(context, "No Transactions Found", Toast.LENGTH_LONG)
+                            .show()
+                    else {
+                        Toast.makeText(
+                            context,
+                            "Downloading Transaction",
+                            Toast.LENGTH_LONG
+                        ).show()
+                        viewModel.generateTransactionPdf(resource.data)
+                    }
+                }
+            }
+
+        }
+    }
+
+    TransactionHistoryDesign(
+        startDate = startDate,
+        onStartDateChange = {
+            startDate = it
+        },
+        startDateError = startDateError,
+        endDate = endDate,
+        onEndDateChange = {
+            endDate = it
+        },
+        endDateError = endDateError,
+        onSelectDates = {
+            val picker = MaterialDatePicker.Builder.dateRangePicker()
+                .setTitleText("Select Date Range")
+                .build()
+
+            picker.show(rememberFragmentManager(), "TransactionFragment")
+
+            picker.addOnPositiveButtonClickListener {
+                calenderStartDate = (it.first / 1000).toString()
+                calenderEndDate = (it.second / 1000).toString()
+
+                startDate = viewModel.convertTimeToDate(it.first)
+                endDate = viewModel.convertTimeToDate(it.second)
+                Log.d("HistoryFragment", it.toString())
+            }
+
+            picker.addOnNegativeButtonClickListener {
+                picker.dismiss()
+            }
+        },
+        onDownload = {
+            if (startDate.isEmpty() && endDate.isEmpty()) {
+                Toast.makeText(context, "Select start and end date", Toast.LENGTH_LONG)
+                    .show()
+                startDateError = "Select Start Date"
+                endDateError = "Select End Date"
+            } else {
+                startDateError = null
+                endDateError = null
+                user?.let {
+                    viewModel.getTransactionHistory(
+                        it.uid,
+                        calenderStartDate,
+                        calenderEndDate
+                    )
+                }
+            }
+        },
+        isLoading = isLoading,
+    )
+
+}
+
+@Composable
+fun TransactionHistoryDesign(
+    startDate: String,
+    onStartDateChange: (String) -> Unit,
+    startDateError: String?,
+    endDate: String,
+    onEndDateChange: (String) -> Unit,
+    endDateError: String?,
+    onSelectDates: @Composable () -> Unit,
+    onDownload: () -> Unit,
+    isLoading: Boolean
+) {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(vertical = 10.dp)
+                .verticalScroll(rememberScrollState())
+                .imePadding()
+        ) {
+
+            OutlinedTextField(
+                value = startDate,
+                onValueChange = onStartDateChange,
+                isError = startDateError != null,
+                label = { Text("Start Date") },
+                placeholder = {
+                    Text("Please enter Start Date")
+                },
+                supportingText = {
+                    if (startDateError != null) {
+                        Text(startDateError)
+                    }
+                },
+                singleLine = true,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp),
+            )
+
+            Spacer(modifier = Modifier.height(6.dp))
+
+            OutlinedTextField(
+                value = endDate,
+                onValueChange = onEndDateChange,
+                isError = endDateError != null,
+                label = { Text("End Date") },
+                placeholder = {
+                    Text("Please enter End Date")
+                },
+                supportingText = {
+                    if (endDateError != null) {
+                        Text(endDateError)
+                    }
+                },
+                singleLine = true,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp),
+            )
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            Button(
+                onClick = { onSelectDates },
+                modifier = Modifier
+                    .width(150.dp)
+                    .align(Alignment.CenterHorizontally)
+            ) {
+                Text("Select Dates")
+            }
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            Button(
+                onClick = onDownload,
+                modifier = Modifier
+                    .width(150.dp)
+                    .align(Alignment.CenterHorizontally)
+            ) {
+                Text("Download")
+            }
+
+        }
+        if (isLoading) {
+            CircularProgressIndicator(
+                modifier = Modifier.align(Alignment.Center)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TransactionHistoryPreview() {
+    TransactionHistoryDesign(
+        startDate = "",
+        onStartDateChange = {},
+        startDateError = "",
+        endDate = "",
+        onEndDateChange = {},
+        endDateError = "",
+        onSelectDates = {},
+        onDownload = {},
+        isLoading = false
+    )
+}
--- app/src/main/java/com/example/managerapp/ui/fragments/TransactionHistoryFragment.kt
+++ app/src/main/java/com/example/managerapp/ui/fragments/TransactionHistoryFragment.kt
@@ -8,12 +8,15 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.Toast
+import androidx.compose.ui.platform.ComposeView
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.example.managerapp.R
 import com.example.managerapp.databinding.FragmentTransactionHistoryBinding
 import com.example.managerapp.ui.ManagerActivity
+import com.example.managerapp.ui.composescreens.TransactionHistoryScreen
+import com.example.managerapp.ui.composescreens.TransactionScreen
 import com.example.managerapp.utils.Resource
 import com.example.managerapp.viewmodel.ManagerViewModel
 import com.google.android.material.datepicker.MaterialDatePicker
@@ -25,113 +28,133 @@ import java.util.Calendar
 import java.util.Locale
 import java.util.TimeZone
 
+//
+//@AndroidEntryPoint
+//class TransactionHistoryFragment : Fragment() {
+//
+//    lateinit var binding: FragmentTransactionHistoryBinding
+//    lateinit var viewModel: ManagerViewModel
+//    private lateinit var user:FirebaseUser
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View {
+//        // Inflate the layout for this fragment
+//        binding = FragmentTransactionHistoryBinding.inflate(inflater,container,false)
+//        return binding.root
+//    }
+//
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//
+//        viewModel = (activity as ManagerActivity).viewModel
+//
+//        user = viewModel.getCurrentUser()!!
+//
+//        var startDate = ""
+//        var endDate = ""
+//
+//        val btnClear = requireActivity().findViewById<ImageButton>(R.id.customIcon)
+//
+//        btnClear.setOnClickListener {
+//            startDate = ""
+//            endDate = ""
+//            clearFields()
+//        }
+//
+//        binding.datesBtn.setOnClickListener {
+//            val picker = MaterialDatePicker.Builder.dateRangePicker()
+//                .setTitleText("Select Date Range")
+//                .build()
+//
+//            picker.show(parentFragmentManager,"TransactionFragment")
+//
+//            picker.addOnPositiveButtonClickListener {
+//                startDate = (it.first/1000).toString()
+//                endDate = (it.second/1000).toString()
+//
+//                binding.tvStartDate.setText(viewModel.convertTimeToDate(it.first))
+//                binding.tvEndDate.setText(viewModel.convertTimeToDate(it.second))
+//                Log.d("HistoryFragment",it.toString())
+//            }
+//
+//            picker.addOnNegativeButtonClickListener {
+//                picker.dismiss()
+//            }
+//        }
+//
+//        binding.downloadBtn.setOnClickListener {
+//            if(startDate.isEmpty() && endDate.isEmpty())
+//                Toast.makeText(requireContext(),"Select start and end date",Toast.LENGTH_LONG).show()
+//            else
+//                viewModel.getTransactionHistory(user.uid,startDate,endDate)
+//        }
+//
+//
+//
+//        observeTransactionHistory()
+//    }
+//
+//    private fun clearFields() {
+//        binding.tvStartDate.setText("")
+//        binding.tvEndDate.setText("")
+//    }
+//
+//    private fun observeTransactionHistory() {
+//        viewLifecycleOwner.lifecycleScope.launch {
+//            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+//                viewModel.getTransactionHistoryResult.collect { resource ->
+//                    when (resource) {
+//                        is Resource.Error -> {
+//                            binding.progressBar.visibility = View.GONE
+//                            println("error getting the transactions ========= ")
+//                            Toast.makeText(requireContext(), resource.message, Toast.LENGTH_LONG)
+//                                .show()
+//                        }
+//
+//                        is Resource.Loading -> {
+//                            binding.progressBar.visibility = View.VISIBLE
+//                        }
+//
+//                        is Resource.StandBy -> {
+//                            binding.progressBar.visibility = View.INVISIBLE
+//                        }
+//
+//                        is Resource.Success -> {
+//                            println("got transaction successfully ========= ")
+//                            binding.progressBar.visibility = View.GONE
+//                            if(resource.data!!.isEmpty())
+//                                Toast.makeText(requireContext(),"No Transactions Found",Toast.LENGTH_LONG).show()
+//                            else{
+//                                Toast.makeText(requireContext(),"Downloading Transaction",Toast.LENGTH_LONG).show()
+//                                viewModel.generateTransactionPdf(resource.data)
+//                            }
+//                        }
+//                    }
+//
+//                }
+//            }
+//        }
+//    }
+//
+//}
+
+
 @AndroidEntryPoint
 class TransactionHistoryFragment : Fragment() {
 
-    lateinit var binding: FragmentTransactionHistoryBinding
     lateinit var viewModel: ManagerViewModel
-    private lateinit var user:FirebaseUser
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment
-        binding = FragmentTransactionHistoryBinding.inflate(inflater,container,false)
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
         viewModel = (activity as ManagerActivity).viewModel
-
-        user = viewModel.getCurrentUser()!!
-
-        var startDate = ""
-        var endDate = ""
-
-        val btnClear = requireActivity().findViewById<ImageButton>(R.id.customIcon)
-
-        btnClear.setOnClickListener {
-            startDate = ""
-            endDate = ""
-            clearFields()
-        }
-
-        binding.datesBtn.setOnClickListener {
-            val picker = MaterialDatePicker.Builder.dateRangePicker()
-                .setTitleText("Select Date Range")
-                .build()
-
-            picker.show(parentFragmentManager,"TransactionFragment")
-
-            picker.addOnPositiveButtonClickListener {
-                startDate = (it.first/1000).toString()
-                endDate = (it.second/1000).toString()
-
-                binding.tvStartDate.setText(viewModel.convertTimeToDate(it.first))
-                binding.tvEndDate.setText(viewModel.convertTimeToDate(it.second))
-                Log.d("HistoryFragment",it.toString())
-            }
-
-            picker.addOnNegativeButtonClickListener {
-                picker.dismiss()
-            }
-        }
-
-        binding.downloadBtn.setOnClickListener {
-            if(startDate.isEmpty() && endDate.isEmpty())
-                Toast.makeText(requireContext(),"Select start and end date",Toast.LENGTH_LONG).show()
-            else
-                viewModel.getTransactionHistory(user.uid,startDate,endDate)
-        }
-
-
-
-        observeTransactionHistory()
-    }
-
-    private fun clearFields() {
-        binding.tvStartDate.setText("")
-        binding.tvEndDate.setText("")
-    }
-
-    private fun observeTransactionHistory() {
-        viewLifecycleOwner.lifecycleScope.launch {
-            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.getTransactionHistoryResult.collect { resource ->
-                    when (resource) {
-                        is Resource.Error -> {
-                            binding.progressBar.visibility = View.GONE
-                            println("error getting the transactions ========= ")
-                            Toast.makeText(requireContext(), resource.message, Toast.LENGTH_LONG)
-                                .show()
-                        }
-
-                        is Resource.Loading -> {
-                            binding.progressBar.visibility = View.VISIBLE
-                        }
-
-                        is Resource.StandBy -> {
-                            binding.progressBar.visibility = View.INVISIBLE
-                        }
-
-                        is Resource.Success -> {
-                            println("got transaction successfully ========= ")
-                            binding.progressBar.visibility = View.GONE
-                            if(resource.data!!.isEmpty())
-                                Toast.makeText(requireContext(),"No Transactions Found",Toast.LENGTH_LONG).show()
-                            else{
-                                Toast.makeText(requireContext(),"Downloading Transaction",Toast.LENGTH_LONG).show()
-                                viewModel.generateTransactionPdf(resource.data)
-                            }
-                        }
-                    }
-
-                }
+        return ComposeView(requireContext()).apply {
+            setContent {
+                TransactionHistoryScreen(viewModel)
             }
         }
     }
-
 }

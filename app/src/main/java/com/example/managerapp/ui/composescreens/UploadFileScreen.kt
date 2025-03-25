8225cd12f5daa81f24cf966a98cc580450d594ed
Meet Mehta <mehtameetoff2802@gmail.com>
2025-03-25 19:00:54 +0530

    started upload file logic

--- app/src/main/java/com/example/managerapp/ui/composescreens/UploadFileScreen.kt
+++ app/src/main/java/com/example/managerapp/ui/composescreens/UploadFileScreen.kt
@@ -1,5 +1,11 @@
 package com.example.managerapp.ui.composescreens
 
+import android.content.Context
+import android.net.Uri
+import android.provider.OpenableColumns
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -22,13 +28,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.managerapp.models.Item
 import com.example.managerapp.viewmodel.ManagerViewModel
+import dagger.hilt.android.internal.Contexts
+
 
 @Composable
 fun UploadFileScreen(viewModel: ManagerViewModel) {
@@ -38,15 +49,92 @@ fun UploadFileScreen(viewModel: ManagerViewModel) {
     var fileNameError by rememberSaveable { mutableStateOf<String?>(null) }
     var fileTypeError by rememberSaveable { mutableStateOf<String?>(null) }
     var fileTypeDropdownExpanded by rememberSaveable { mutableStateOf(false) }
+    var csvUri by rememberSaveable { mutableStateOf<Uri?>(null) }
     var isLoading by rememberSaveable { mutableStateOf(false) }
+
+    val context = LocalContext.current
+
+
+    fun processCSV(context: Context, uri: Uri) {
+        val contentResolver = context.contentResolver
+
+        try {
+            val inputStream = contentResolver.openInputStream(uri)
+            inputStream?.bufferedReader()?.use { reader ->
+                val csvData = mutableListOf<Item>()
+
+                reader.readLine()
+                reader.forEachLine { line ->
+                    val tokens = line.split(",")
+                    if (tokens.size >= 3) {
+                        val item = Item(
+                            item_id = tokens[0],
+                            item_name = tokens[1],
+                            item_stock = tokens[2].toIntOrNull() ?: 0
+                        )
+                        csvData.add(item)
+                    }
+                }
+
+            }
+
+        } catch (e: Exception) {
+            Toast.makeText(context, "Error reading CSV", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun getFileNameFromUri(uri: Uri): String {
+        val cursor = context.contentResolver.query(uri, null, null, null, null)
+        cursor?.use {
+            if (it.moveToFirst()) {
+                val nameIndex = it.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+                if (nameIndex != -1) {
+                    return it.getString(nameIndex)
+                }
+            }
+        }
+
+        return if (uri.lastPathSegment == null)
+            ""
+        else
+            uri.lastPathSegment.toString() // Fallback
+    }
+
+
+    val filePickerLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent(),
+        onResult = { uri ->
+            if (uri != null) {
+                csvUri = uri
+                fileName = getFileNameFromUri(uri)
+            }
+        }
+    )
 
     fun clearFields() {
         fileType = "File Format"
         fileName = ""
         fileNameError = null
         fileTypeError = null
-//        fileUri = null
+        csvUri = null
 //        csvData = mutableListOf()
+    }
+
+    fun onUpload() {
+        fileTypeError = if (fileType != "csv")
+            "Please select file format"
+        else
+            null
+
+        fileNameError = if (fileName == "")
+            "Please select your file"
+        else
+            null
+
+        if (fileTypeError == null && fileNameError == null) {
+            csvUri?.let { processCSV(context, it) }
+        }
+
     }
 
     UploadFileDesign(
